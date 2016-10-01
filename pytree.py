1856163843fa2879ce16aadb53b5ea5a08c9d44e
Yan-J <yan_j1213@126.com>
2016-10-02 01:04:26 +0800

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -5,40 +5,45 @@ import os
 import string
 import re
 
-def printDir(path, file_cnt, dir_cnt,indent=''):
+def printName(indent, i, lastElement):
+    if i == lastElement:
+        print(indent + '└── ' + str(i))
+        indent1 = indent + '    '
+    else:
+        print(indent + '├── ' + str(i))
+        indent1 = indent + '│   '
+    return indent1
+    
+
+def sortList(curList):
+    curList = [item for item in curList if item[0] != ('.' or '_')]
+    curList = sorted(curList, key=str.lower)
+    return curList
+
+
+def printDir(path, file_cnt, dir_cnt, indent=''):
     if (path[-1] != '/'):
         path = path + '/'
     curList = os.listdir(path)
-    curList = [item for item in curList if item[0] != '.']#remove hidden files
-    curList = sorted(curList, key=str.lower)
+    curList = sortList(curList)
     cnt = []
     for i in curList:
         curPath = path + i
-        if (os.path.isdir(curPath) and i == curList[-1]):
+        if (os.path.isdir(curPath)):
             curPath = curPath + '/'
-            dir_cnt = dir_cnt + 1
-            print(indent + '└── ' + str(i))
-            indent1 = indent + '    '
+            dir_cnt = dir_cnt + 1 
+            indent1 = printName(indent, i, curList[-1])
             cnt1 = printDir(curPath, file_cnt, dir_cnt, indent1)
             file_cnt = cnt1[1]
             dir_cnt = cnt1[0]
-        elif (os.path.isdir(curPath) and i != curList[-1]):
-            curPath = curPath + '/'
-            dir_cnt = dir_cnt + 1
-            print(indent + '├── ' + str(i))
-            indent1 = indent + '│   '
-            cnt1 = printDir(curPath, file_cnt, dir_cnt, indent1)
-            file_cnt = cnt1[1]
-            dir_cnt = cnt1[0]
-        elif (os.path.isfile(curPath) and i == curList[-1]):
-            print(indent + '└── ' + str(i))
+        elif(os.path.isfile(curPath)):
+            printName(indent, i, curList[-1])
             file_cnt = file_cnt + 1
-        elif (os.path.isfile(curPath) and i != curList[-1]):
-            print(indent + '├── ' + str(i))
-            file_cnt = file_cnt + 1    
+              
     cnt.append(dir_cnt)
     cnt.append(file_cnt)
     return cnt
+
 
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
@@ -48,4 +53,4 @@ if __name__ == '__main__':
     print(path)
     cnt = printDir(path, 0, 0, '')
     print()
-    print(str(cnt[0]) + ' directories', str(cnt[1]) + ' files')
+    print('%d directories %d files' % (cnt[0], cnt[1]))
