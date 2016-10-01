3a2f9ee74030de40bde611a2dc4926b7acb0293e
Yan-J <yan_j1213@126.com>
2016-10-02 01:15:04 +0800

    Update pytree.py

--- pytree.py
+++ pytree.py
@@ -5,6 +5,7 @@ import os
 import string
 import re
 
+
 def printName(indent, i, lastElement):
     if i == lastElement:
         print(indent + '└── ' + str(i))
@@ -13,11 +14,15 @@ def printName(indent, i, lastElement):
         print(indent + '├── ' + str(i))
         indent1 = indent + '│   '
     return indent1
+
+
+def sort_key(s):
+    return re.sub('[^A-Za-z]+', '', s).lower()
     
 
 def sortList(curList):
     curList = [item for item in curList if item[0] != ('.' or '_')]
-    curList = sorted(curList, key=str.lower)
+    curList = sorted(curList, key=sort_key)
     return curList
 
 
@@ -31,7 +36,7 @@ def printDir(path, file_cnt, dir_cnt, indent=''):
         curPath = path + i
         if (os.path.isdir(curPath)):
             curPath = curPath + '/'
-            dir_cnt = dir_cnt + 1 
+            dir_cnt = dir_cnt + 1
             indent1 = printName(indent, i, curList[-1])
             cnt1 = printDir(curPath, file_cnt, dir_cnt, indent1)
             file_cnt = cnt1[1]
@@ -39,7 +44,6 @@ def printDir(path, file_cnt, dir_cnt, indent=''):
         elif(os.path.isfile(curPath)):
             printName(indent, i, curList[-1])
             file_cnt = file_cnt + 1
-              
     cnt.append(dir_cnt)
     cnt.append(file_cnt)
     return cnt
@@ -50,7 +54,9 @@ if __name__ == '__main__':
         path = '.'
     elif (len(sys.argv) == 2):
         path = sys.argv[1]
-    print(path)
-    cnt = printDir(path, 0, 0, '')
-    print()
-    print('%d directories %d files' % (cnt[0], cnt[1]))
+        print(path)
+        cnt = printDir(path, 0, 0, '')
+        print()
+        print('%d directories, %d files' % (cnt[0], cnt[1]))
+    else:
+        print('Invalid arguments.')
